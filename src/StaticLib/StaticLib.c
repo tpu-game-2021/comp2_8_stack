8a764200c78cb1efa173c3aeaf44d657afebac1a
zakiAKR <zakican.ak@gmail.com>
2021-11-07 22:59:43 +0900

    テストコード通りませんでした。

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -9,33 +9,46 @@
 void initialize(STACK* s, size_t mem_size)
 {
 	if (s == NULL) return;
-
+	
 	s->stack_pointer = NULL;
-	s->stack_memory = NULL;
+	//s->stack_pointer = mem_size;
+	s->stack_memory= NULL;
 	s->end = NULL;
 }
-
 
 // 確保したメモリを解放する
 void finalize(STACK* s)
 {
 	// ToDo: Initializeで確保したメモリを解放しよう
+	if (s == NULL) return;
+	free(s->stack_pointer);
+	s->stack_pointer = NULL;
+	s->stack_memory= NULL;
+	s->end = NULL;
 }
-
 
 // valの値をスタックに積む。実行の成否を返す
 bool push(STACK* s, int val)
 {
 	// ToDo: valの値をスタックに保存しよう
-	return false;
+	s->stack_memory[0] = sizeof s->stack_memory / sizeof * s->stack_memory;
+	s->stack_memory[--s->stack_memory[0]] = val;
+	s->stack_pointer[--s->stack_memory[0]] = val;
+	return true;
+	
 }
-
 
 // addrから始まるnum個の整数をスタックに積む。実行の成否を返す
 bool push_array(STACK* s, int* addr, int num)
 {
 	// ToDo: addrからはじまるnum個の整数をスタックに保存しよう
-	return false;
+	s->stack_memory[0] = sizeof s->stack_memory / sizeof * s->stack_memory;
+	s->stack_pointer[0] = sizeof s->stack_pointer / sizeof * s->stack_pointer;
+	for (int i = 0; i < num; i++) {
+		s->stack_memory[--s->stack_memory[0]] = addr[i];
+		s->stack_pointer[--s->stack_pointer[0]] = addr[i];
+	}
+	return true;
 }
 
 // スタックから一つの要素を取り出す
@@ -43,14 +56,20 @@ int pop(STACK* s)
 {
 	// ToDo: スタックの最上位の値を取り出して返そう
 	// 不具合時は0を返す
-	return 0;
+	s->stack_memory[0] = sizeof s->stack_memory / sizeof * s->stack_memory;
+	return s->stack_memory[s->stack_memory[0]++];
 }
 
 // addrにスタックからnumの要素を取り出す。取り出せた個数を返す
 int pop_array(STACK* s, int* addr, int num)
 {
 	// ToDo: スタックからnum個の値を取り出してaddrから始まるメモリに保存しよう
-	// スタックにnum個の要素がたまっていなかたら、積まれている要素を返して、
+	// スタックにnum個の要素がたまっていなかったら、積まれている要素を返して、
 	// 積んだ要素数を返り値として返そう
-	return 0;
+	s->stack_memory[0] = sizeof s->stack_memory / sizeof * s->stack_memory;
+	for (int i = 0; i < num; i++) {
+		addr[i] = s->stack_memory[s->stack_memory[0]++];
+		addr[i] = s->stack_pointer[s->stack_pointer[0]++];
+	}
+	return s->stack_memory[s->stack_memory[0]++];
 }
