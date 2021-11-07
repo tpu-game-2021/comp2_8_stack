c22731d9d9723fce24f46228edd0b4bdd0c47a6f
matsuurataketo <g2127063@st.t-kougei.ac.jp>
2021-11-07 23:15:55 +0900

    テストが通りませんでした

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -19,31 +19,70 @@ void initialize(STACK* s, size_t mem_size)
 // 確保したメモリを解放する
 void finalize(STACK* s)
 {
+
 	// ToDo: Initializeで確保したメモリを解放しよう
+	if (s->stack_pointer == NULL) return;
+	free(s->stack_pointer);
+	s->stack_pointer = NULL;
+	s->stack_memory = NULL;
+	s->end = NULL;
 }
 
 
 // valの値をスタックに積む。実行の成否を返す
 bool push(STACK* s, int val)
 {
+	if (s == NULL) { return false; }
+	if (s->stack_memory == NULL) {
+		s->stack_memory = val;
+		s->stack_pointer = val;
+		s->end = val;
+		return true;
 	// ToDo: valの値をスタックに保存しよう
 	return false;
+	}
+	s->stack_pointer++;
+	s->stack_pointer = val;
+	s->end = val;
+	return true;
 }
 
 
 // addrから始まるnum個の整数をスタックに積む。実行の成否を返す
 bool push_array(STACK* s, int* addr, int num)
 {
+
 	// ToDo: addrからはじまるnum個の整数をスタックに保存しよう
-	return false;
+
+	if (s == NULL)return false;
+	if (addr == NULL)return false;
+	for (int i = 0; i < num; i++)
+	{
+		push(s, addr[i]);
+	}
+	return true;
 }
 
 // スタックから一つの要素を取り出す
 int pop(STACK* s)
 {
+	int x;
 	// ToDo: スタックの最上位の値を取り出して返そう
 	// 不具合時は0を返す
-	return 0;
+
+	if (s == NULL)return 0;
+	if (s->stack_memory == NULL)return 0;
+	x = s->end;
+	s->stack_pointer = NULL;
+	if (s->end == s->stack_memory)
+	{
+		s->stack_memory = NULL;
+		s->end = NULL;
+		return x;
+	}
+	s->stack_pointer--;
+	s->end = s->stack_pointer;
+	return x;
 }
 
 // addrにスタックからnumの要素を取り出す。取り出せた個数を返す
@@ -52,5 +91,15 @@ int pop_array(STACK* s, int* addr, int num)
 	// ToDo: スタックからnum個の値を取り出してaddrから始まるメモリに保存しよう
 	// スタックにnum個の要素がたまっていなかたら、積まれている要素を返して、
 	// 積んだ要素数を返り値として返そう
-	return 0;
+	int count = 0;
+	if (s == NULL)return false;
+	if (addr == NULL)return false;
+	for (int i = 0; i < num; i++)
+	{
+		if (s->stack_memory == NULL)break;
+		addr = pop(s);
+		addr++;
+		count++;
+	}
+	return count;
 }
